bf4909ced37444e62ec4a7bb4c7b3a81eeea3bf1
Greg <sunerg51@students.rowan.edu>
2014-11-23 17:34:51 -0500

    Second Commit
    -Readjusted format to fit Player interface

--- feature_Player.py
+++ feature_Player.py
@@ -1,19 +1,18 @@
  # Greg Suner and Alex Ciarmella
  # Concrete Player Class
 
- class Player:
+ class GSACPlayer(Player.Player):
  
 	def __init__(self):
-	
+		self.name - None
 	#Decides next move based on opponents past moves
-	#For now, assumes a list parameter that has past moves in a "player, opponent, player,..." order
-	#Looks for what opponent throws the most and tries to counter it
-	#0-rock, 1-paper, 2-scissors
+	#Looks for what opponent throws the most and tries to counter it. 0-rock, 1-paper, 2-scissors
+	#Takes list of opponents past moves
 	def play(self, pastmoves):
 		rock, paper, scissors = 0
 		
 		for index, move in enumerate(pastmoves):
-			if index % 2 == 1:
+
 				if move == 0:
 					rock += 1
 				elif move == 1:
@@ -21,16 +20,20 @@
 				else
 					scissors += 1
 		else
-			#Not sure how to output a move but for now the first move is just rock since the list is empty
-			#Throw rock
+			return 0
 		
 		#Look for most thrown move and throw counter
 		if rock > paper && rock > scissors:
-			#Throw paper
+			return 0
 		elif paper > rock && paper > scissors:
-			#Throw scissors
+			return 2
 		elif scissors > rock && scissors > paper:
-			#Throw paper
+			return 1
 		else #Arbitrary throw if 2 or 3 are thrown evenly
-			#Throw rock
+			return 0
+			
+	def get_name(self)
+		return self.name
 		
+	def set_name (self, playername):
+		self.name = playername
